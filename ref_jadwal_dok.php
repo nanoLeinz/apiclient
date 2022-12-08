05f545ab65f63222a918d621fdcd35e395d5dd0d
nanoleinz <nanoleinz@gmail.com>
2022-12-08 08:58:22 +0700

    little edit

--- ref_jadwal_dok.php
+++ ref_jadwal_dok.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'conf.php';
 
+
+
 $response = $client->request('GET', 'jadwaldokter/kodepoli/INT/tanggal/2022-11-03', [
     'headers' => getSignature()
 ]);
@@ -9,5 +11,5 @@ $kunci = getKey();
 $en = json_decode($response->getBody()->getContents());
 $es = decompress(stringDecrypt($kunci, $en->response));
 print_r($es);
-
+// var_dump(getSignature(),"/n",getKey())
 ?>
